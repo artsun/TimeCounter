43c4225b74cedc91275b139ab715612f519da2f7
artsun <parafo@gmail.com>
2020-07-17 23:11:01 +0300

    Update views.py

--- views.py
+++ views.py
@@ -2,21 +2,24 @@
 
 from flask import request, render_template, redirect, Blueprint, flash, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import login_user, login_required, current_user, logout_user
+from flask_login import login_user, login_required, current_user, logout_user, AnonymousUserMixin
 
 from .models import User
 from . import db
 
 
-indx = Blueprint('indx', __name__)
+common = Blueprint('common', __name__)
 
 
-@indx.route('/')
-def indx_page():
-    return render_template('index.html')
+@common.route('/')
+def main_page():
+    cuser = '' if not current_user.is_active and not current_user.is_authenticated and current_user.is_anonymous else current_user.name
+
+    print()
+    return render_template('calend.html', name=cuser, ishold=False)
 
 
-@indx.route('/profile')
+@common.route('/profile')
 @login_required
 def profile_page():
     if request.method == 'GET':
@@ -25,19 +28,19 @@ def profile_page():
     else:
         res = 'Incorrect request'
 
-    return render_template('calend.html', name=current_user.name, ishold=False)
+    return render_template('profile.html', name=current_user.name)
 
 
 auth = Blueprint('auth', __name__)
 
 
 @auth.route('/login')
-def login():
+def login_page():
     return render_template('login.html')
 
 
 @auth.route('/login', methods=['POST'])
-def login_post():
+def login_post_page():
     email = request.form.get('email')
     password = request.form.get('password')
     remember = True if request.form.get('remember') else False
@@ -45,20 +48,20 @@ def login_post():
     user = User.query.filter_by(email=email).first()
 
     if not user or not check_password_hash(user.password, password):
-        flash('Please check your login details and try again.')
-        return redirect(url_for('auth.login'))
+        flash('Введите корректные данные')
+        return redirect(url_for('auth.login_page'))
 
     login_user(user, remember=remember)
-    return redirect(url_for('indx.profile_page'))
+    return redirect(url_for('common.main_page'))
 
 
 @auth.route('/signup')
-def signup():
+def signup_page():
     return render_template('signup.html')
 
 
 @auth.route('/signup', methods=['POST'])
-def signup_post():
+def signup_post_page():
     email = request.form.get('email')
     name = request.form.get('name')
     password = request.form.get('password')
@@ -67,18 +70,18 @@ def signup_post():
     user = User.query.filter_by(email=email).first()
 
     if user:
-        flash('Email address already exists')
-        return redirect(url_for('auth.signup'))
+        flash('Данная почта уже зарегистрирована')
+        return redirect(url_for('auth.signup_page'))
 
     new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
 
     db.session.add(new_user)
     db.session.commit()
 
-    return redirect(url_for('auth.login'))
+    return redirect(url_for('auth.login_page'))
 
 
 @auth.route('/logout')
-def logout():
+def logout_page():
     logout_user()
-    return redirect(url_for('indx.indx_page'))
+    return redirect(url_for('common.main_page'))
