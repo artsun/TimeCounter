dc1f2ca93ba36fdf43fb30a7f9190208c95efdcd
artsun <parafo@gmail.com>
2020-07-18 17:23:51 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,28 +1,36 @@
 
 from flask_login import UserMixin
 from datetime import datetime
+from uuid import uuid4
+from sqlalchemy_utils import UUIDType
+
+
 from . import db
 
 
 class User(UserMixin, db.Model):
-    pk = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), unique=True)
+    pk = db.Column(UUIDType(binary=True), primary_key=True, default=uuid4, unique=True, nullable=False)
+    email = db.Column(db.String(100))
     password = db.Column(db.String(100))
-    name = db.Column(db.String(1000))
+    name = db.Column(db.String(1000), unique=True)
 
     def get_id(self):
         return self.pk
 
 
-class Day(db.Model):
-    pk = db.Column(db.Integer, primary_key=True)
-    user_pk = db.Column(db.Integer, db.ForeignKey('user.pk'))
+class Wday(db.Model):
+    pk = db.Column(UUIDType(binary=True), primary_key=True, unique=True, nullable=False)
+    user_pk = db.Column(UUIDType(binary=True), db.ForeignKey('user.pk'))
     start = db.Column(db.DateTime, default=datetime.now)
     longitude = db.Column(db.Integer())
+    day = db.Column(db.Integer, default=datetime.now().day)
+    month = db.Column(db.Integer, default=datetime.now().month)
+    year = db.Column(db.Integer, default=datetime.now().year)
+    finish = db.Column(db.DateTime)
 
 
 class Break(db.Model):
-    pk = db.Column(db.Integer, primary_key=True)
-    day_pk = db.Column(db.Integer, db.ForeignKey('day.pk'))
+    pk = db.Column(UUIDType(binary=True), primary_key=True, unique=True, nullable=False)
+    day_pk = db.Column(UUIDType(binary=True), db.ForeignKey('wday.pk'))
     start = db.Column(db.DateTime, default=datetime.now)
     stop = db.Column(db.DateTime, default=datetime.now)
