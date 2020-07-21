5cee7b26437dfbbfebc0433d3e3a2df9d0d89338
artsun <parafo@gmail.com>
2020-07-21 23:11:52 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,19 +2,22 @@ import os.path
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-import base64
-
+from flask_caching import Cache
 
 db = SQLAlchemy()
-
+cache = Cache(config={'CACHE_TYPE': 'simple'})
 
 def create_app():
+
+
+
     app = Flask(__name__,  static_url_path='', static_folder='static',)
     app.config['CSRF_ENABLED']= True
     app.config['SECRET_KEY'] = 'any-key'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{}'.format(os.path.join(os.path.dirname(__file__), 'storage.db'))
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+    cache.init_app(app)
     db.init_app(app)
 
     from .models import User
@@ -25,7 +28,6 @@ def create_app():
 
     @login_manager.user_loader
     def load_user(user_pk):
-        print('USER_PK', user_pk)
         return User.query.get(user_pk) if not isinstance(user_pk, int) else None
 
     from .views import auth
