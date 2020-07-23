a2a08a86b25d188e4743e3221fa18152ebacc286
artsun <parafo@gmail.com>
2020-07-23 21:21:37 +0300

    Update tools.py

--- tools.py
+++ tools.py
@@ -1,12 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from flask_login import current_user
 from datetime import timedelta
-
-
-def define_current_user(current_user: current_user) -> str:
-    is_anon = (current_user.is_active, current_user.is_authenticated, current_user.is_anonymous)
-    return '' if is_anon == (False, False, True) else current_user.name
 
 
 def delta_to_hms(delta: timedelta) -> str:
