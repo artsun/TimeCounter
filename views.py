e01525b54c8cd3ff31ce423a3aa2375d9def81ea
artsun <parafo@gmail.com>
2020-07-31 15:44:24 +0300

    Update views.py

--- views.py
+++ views.py
@@ -1,165 +1,129 @@
-# -*- coding: utf-8 -*-
 
-from flask import request, render_template, redirect, Blueprint, flash, url_for, make_response
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import login_user, login_required, current_user, logout_user
-from datetime import datetime
-from collections import namedtuple
+from flask_login import UserMixin
+from datetime import datetime, timedelta
+from uuid import uuid4
+from sqlalchemy_utils import UUIDType
+from sqlalchemy.orm import relationship
+from calendar import monthrange
+from flask_login import current_user
 
-from .constants import MONTHS
-from .tools import delta_to_hms
-from .models import User, Wday, Break
 from . import db
-from . import cache
-
-HMS = namedtuple('HMS', ['start', 'stop', 'duration', 'done'])
-
-common = Blueprint('common', __name__)
+from .constants import MONTHS
 
 
-@common.route('/', methods=['GET'])
-def main_page():
-    cuser = User.check_anon(current_user)
-    day = cuser.day(tuple(request.args.values())) if request.args else cuser.day()
+class User(UserMixin, db.Model):
+    pk = db.Column(UUIDType(binary=False), primary_key=True, default=uuid4, unique=True, nullable=False)
+    email = db.Column(db.String(100))
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000), unique=True)
+    days = relationship("Wday", cascade="all,delete", backref="user")
 
-    if day is not None:
-        print(Wday().get_breaks())
-        cache.set(cuser.pk, day.pk)
-        break_now = Break.today(day).filter_by(actual=True).first()
-        is_pause = 1 if break_now else 0
-        month_start, month_stop = MONTHS[day.start.month], MONTHS[day.finish.month]
-        today = HMS(day.start, day.finish, delta_to_hms(day.finish-day.start), day.done)
-        breaks_sum = day.calc_breaks()
-        breaks_sum = delta_to_hms(breaks_sum) if breaks_sum else ''
-    else:
-        is_pause, today, breaks_sum, month_start, month_stop = 0, None, '', '', ''
+    def get_id(self):
+        return self.pk
 
-    return render_template('calend.html', cuser=cuser, breaks_sum=breaks_sum, is_pause=is_pause, today=today,
-                           month_start=month_start, month_stop=month_stop)
+    def day(self, dmy: tuple = None):
+        dmy = (f'{datetime.now().day}', f'{datetime.now().month}', f'{datetime.now().year}') if dmy is None else dmy
+        day = [day.check_fin() for day in self.days if (f'{day.day}', f'{day.month}', f'{day.year}') == dmy]
+        return day[0] if len(day) == 1 else None
+
+    @staticmethod
+    def check_anon(current_user: current_user) -> current_user:
+        is_anon = (current_user.is_active, current_user.is_authenticated, current_user.is_anonymous)
+        return User() if is_anon == (False, False, True) else current_user
 
 
-@common.route('/refreshtimer', methods=['GET'])
-def refresh_timer():
-    if request.args.get('do') is not None:
-        cuser = User.check_anon(current_user)
-        day = cuser.day() if cache.get(cuser.pk) is None else Wday.by_pk(cache.get(cuser.pk))
-        if day is not None:
-            left = day.left()
-            left['now'], left['max'] = (day.finish - datetime.now()).seconds, (day.finish - day.start).seconds
+class Wday(db.Model):
+    pk = db.Column(UUIDType(binary=False), primary_key=True, default=uuid4, unique=True, nullable=False)
+    user_pk = db.Column(UUIDType(binary=False), db.ForeignKey('user.pk'))
+    start = db.Column(db.DateTime, default=datetime.now)
+    longitude = db.Column(db.Integer())
+    day = db.Column(db.Integer, default=datetime.now().day)
+    month = db.Column(db.Integer, default=datetime.now().month)
+    year = db.Column(db.Integer, default=datetime.now().year)
+    finish = db.Column(db.DateTime)
+    done = db.Column(db.Boolean, default=False)
+    breaks = relationship("Break", cascade="all,delete", backref="wday")
+
+    def check_fin(self):
+        if datetime.now() > self.finish:
+            self.done = True
+            self.update_session()
+        return self
+
+    def recalc_longitude(self) -> int:
+        delta = (self.finish-self.start)
+        return delta.seconds//3600
+
+    def calc_finish(self):
+        h = self.start.hour + self.longitude
+        # TODO:  предусмотреть год
+        if (h // 24) and monthrange(self.start.year, self.start.month)[1] == self.start.day:
+            month, days = self.start.month + 1, 1
         else:
-            left = dict(getHours=0, getMinutes=0, getSeconds=0, now=0, max=100)
-        return left
+            days = (h // 24) + self.start.day
+            month = self.start.month
+        h = h % 24 if h // 24 else h
+        return self.start.replace(hour=h, day=days, month=month) + self.calc_breaks()
+
+    def calc_breaks(self) -> timedelta:
+        breaks = Break.today(self).filter_by(actual=False)
+        return sum(((x.stop - x.start) for x in breaks), timedelta()) if breaks else timedelta(0,0)
+
+    def get_breaks(self):
+        nonact, act = self.breaks.copy(), []
+        [act.append(nonact.pop(n)) for n, br in enumerate(nonact) if br.actual]
+        return nonact, act
+
+    def breaks_strings(self):
+        nonact, act = self.get_breaks()
+        fmt = ('%H', '%M', '%S')
+        breaks = [f'{el.wday.day} {MONTHS[el.wday.month]} ' + ':'.join([f"{el.start.strftime(fm)}" for fm in fmt]) +
+                  ' - ' + ':'.join([f"{el.stop.strftime(fm)}" for fm in fmt]) + " ({} ч. {} мин. {} сек.)". \
+                      format(*el.period()) for el in nonact]
+        breaks += [
+            f'{el.wday.day} {MONTHS[el.wday.month]} ' + ':'.join([f"{el.start.strftime(fm)}" for fm in fmt]) + ' -' for
+            el in act]
+        return breaks
+
+    def left(self) -> dict:
+        break_ = Break.today(self).filter_by(actual=True).first()
+        diff = break_.start if break_ is not None else datetime.now()
+        left = (self.finish - diff) if self.finish and self.finish > diff else timedelta(0, 0)
+        return dict(getHours=left.seconds//3600, getMinutes=(left.seconds//60)%60, getSeconds=left.seconds%60)
+
+    @staticmethod
+    def by_user_today(user: User):
+        return None if user is None else Wday.query.filter_by(user_pk=user.pk, day=datetime.now().day).first()
+
+    @staticmethod
+    def by_pk(pk):
+        day = Wday.query.filter_by(pk=pk).first()
+        return Wday() if day is None else day
+
+    def update_session(self):
+        db.session.add(self)
+        db.session.commit()
 
 
-@common.route('/refreshbreaks', methods=['GET'])
-def refresh_breaks():
-    if request.args.get('do') is not None:
-        cuser = User.check_anon(current_user)
-        day = cuser.day() if cache.get(cuser.pk) is None else Wday.by_pk(cache.get(cuser.pk))
-        res = day.breaks_strings() if day is not None else ''
-        return dict(breaks=res)
+class Break(db.Model):
+    pk = db.Column(UUIDType(binary=False), primary_key=True, default=uuid4, unique=True, nullable=False)
+    day_pk = db.Column(UUIDType(binary=False), db.ForeignKey('wday.pk'))
+    start = db.Column(db.DateTime, default=datetime.now)
+    stop = db.Column(db.DateTime)
+    actual = db.Column(db.Boolean)
 
+    @staticmethod
+    def today(day):
+        return Break.query.filter_by(day_pk=day.pk) if day is not None else None
 
-@common.route('/setday', methods=['GET'])
-def setday():
-    cuser = User.check_anon(current_user)
-    day = Wday.by_user_today(cuser)
-    if request.args.get("paused") is not None:
-        if day is not None and day.finish > datetime.now():
-            actual_break = Break.today(day).filter_by(actual=True).first()
-            if actual_break:  # уже на паузе
-                actual_break.close()
-            else:
-                db.session.add(Break(day_pk=day.pk, actual=True))
-                db.session.commit()
-        return dict(done='ok')
-    if request.args.get("started") is not None:
-        return dict(started=1) if day is not None and day.finish > datetime.now() else dict(started=0)
+    def period(self) -> tuple:
+        delta = self.stop - self.start
+        return delta.seconds//3600, (delta.seconds//60)%60, delta.seconds%60
 
-
-@common.route('/', methods=['POST'])
-def set_day():
-    print(request.form)
-    cuser = User.check_anon(current_user)
-    day = Wday.by_user_today(cuser)
-    if request.form.get("begindStore") is not None:
-        if cuser and day is None:
-            begind = int(request.form.get("begindStore"))
-            day = Wday(user_pk=cuser.pk, longitude=begind)
-            day.update_session()
-            day.finish = day.calc_finish()
-            day.update_session()
-    if request.form.get("changedStore") is not None:
-        if day is not None and day.finish > datetime.now():
-            day.longitude = int(request.form.get("changedStore"))
-            day.update_session()
-            day.finish = day.calc_finish()
-            day.update_session()
-    if request.form.get("finishd") is not None:
-        if day is not None:
-            day.finish = datetime.now()
-            day.done = True
-            day.update_session()
-            day.longitude = day.recalc_longitude()
-            day.update_session()
-    if request.form.get("resetd") is not None:
-        if day is not None:
-            db.session.delete(day)
-            db.session.commit()
-    return redirect(url_for('common.main_page'))
-
-
-@common.route('/profile')
-@login_required
-def profile_page():
-    return render_template('profile.html', cuser=current_user)
-
-
-auth = Blueprint('auth', __name__)
-
-
-@auth.route('/login')
-def login_page():
-    return render_template('login.html')
-
-
-@auth.route('/login', methods=['POST'])
-def login_post_page():
-    username = request.form.get('username')
-    password = request.form.get('password')
-    remember = True if request.form.get('remember') else False
-
-    user = User.query.filter_by(name=username).first()
-
-    if not user or not check_password_hash(user.password, password):
-        flash('Введён неверный пароль, либо пользователь не существует')
-        return redirect(url_for('auth.login_page'))
-
-    login_user(user, remember=remember)
-    return redirect(url_for('common.main_page'))
-
-
-@auth.route('/signup')
-def signup_page():
-    return render_template('signup.html')
-
-
-@auth.route('/signup', methods=['POST'])
-def signup_post_page():
-    email, name, password = request.form.get('email'), request.form.get('name'), request.form.get('password')
-    user = User.query.filter_by(name=name).first()
-
-    if user:
-        flash('Пользователь уже зарегистрирован')
-        return redirect(url_for('auth.signup_page'))
-
-    db.session.add(User(email=email, password=generate_password_hash(password, method='sha256'), name=name))
-    db.session.commit()
-
-    return redirect(url_for('auth.login_page'))
-
-
-@auth.route('/logout')
-def logout_page():
-    logout_user()
-    return redirect(url_for('common.main_page'))
+    def close(self):
+        self.stop = datetime.now()
+        self.wday.finish += (self.stop - self.start)
+        self.wday.update_session()
+        self.actual = False
+        db.session.add(self)
+        db.session.commit()
